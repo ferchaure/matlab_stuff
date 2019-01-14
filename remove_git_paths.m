02488f417189bdf3c85b97b8de2d96d95314537a
Fernando J. Chaure <ferchaure@users.noreply.github.com>
2019-01-14 17:39:18 -0300

    add remove_git_paths

---
+++ remove_git_paths.m
@@ -0,0 +1,11 @@
+all_path = strsplit(path,';');
+counter = 0;
+for p= all_path
+    
+    if contains(p,'.git')
+        counter = 1 +counter;
+        rmpath(p{1})
+    end
+    
+end
+fprintf('%d files removed from path\n',counter)
