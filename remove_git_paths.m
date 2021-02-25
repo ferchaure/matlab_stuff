0d39539e22027906b29a6e2caa977b50bd8552ae
Fernando J. Chaure <ferchaure@users.noreply.github.com>
2021-02-25 12:51:23 -0300

    Update remove_git_paths.m

--- remove_git_paths.m
+++ remove_git_paths.m
@@ -1,11 +1,13 @@
-all_path = strsplit(path,';');
-counter = 0;
-for p= all_path
-    
-    if contains(p,'.git')
-        counter = 1 +counter;
-        rmpath(p{1})
+function remove_git_paths()
+    all_path = strsplit(path,';');
+    counter = 0;
+    for p= all_path
+
+        if contains(p,'.git')
+            counter = 1 +counter;
+            rmpath(p{1})
+        end
+
     end
-    
+    fprintf('%d files removed from path\n',counter)
 end
-fprintf('%d files removed from path\n',counter)
